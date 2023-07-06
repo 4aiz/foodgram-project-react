1d2dbef54e00973aac88218eb041571b737f816f
4aiz <milkocan@mail.ru>
2023-07-06 19:59:10 +0300

    part 1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -30,7 +30,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['158.160.22.0', '127.0.0.1', 'localhost', '4aizFoodgram.hidehost.me']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -79,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -114,7 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -127,7 +124,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
